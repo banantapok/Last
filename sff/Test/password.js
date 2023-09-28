e69c0299cbf5312064e0e7fa5227fcd4d3210439
banantapok <130862930+banantapok@users.noreply.github.com>
2023-09-28 13:06:36 +0300

    Create password.js

---
+++ sff/Test/password.js
@@ -0,0 +1,37 @@
+// password.js
+
+var allowedPasswords = [
+  "EGBAK_Eg7619",
+  "Hao729_LIPA",
+  // Другие разрешенные пароли
+];
+
+var loginEnabled = true; // По умолчанию вход разрешен
+
+function PassCheck() {
+  if (loginEnabled) {
+    var a = document.getElementById("pass").value;
+    
+    if (allowedPasswords.includes(a)) {
+      var image = document.getElementById("imgggg");
+      image.src = "https://banantapok.github.io/premium/autch/check.png";
+      window.location.href = "https://banantapok.github.io/Last/sff/QQ.html";
+    } else {
+      var image = document.getElementById("imgggg");
+      image.src = "https://banantapok.github.io/premium/autch/remove.png";
+      document.write(" ");
+    }
+  } else {
+    alert("Вход временно приостановлен. Подождите.");
+  }
+}
+
+function enableLogin() {
+  loginEnabled = true; // Установить вход разрешенным
+  alert("Вход включен.");
+}
+
+function disableLogin() {
+  loginEnabled = false; // Установить вход временно приостановленным
+  alert("Вход отключен. Подождите.");
+}
